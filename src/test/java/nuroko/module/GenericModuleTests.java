0d7a16a13ce4b1d33e6dbf5cf57f81a0f62dbd0b
mikera <mike.r.anderson.13@gmail.com>
2013-04-02 11:39:42 +0800

    module and weight layer tests

---
+++ src/test/java/nuroko/module/GenericModuleTests.java
@@ -0,0 +1,103 @@
+package nuroko.module;
+
+import mikera.vectorz.AVector;
+import mikera.vectorz.Vectorz;
+import nuroko.core.IParameterised;
+import nuroko.core.IThinker;
+import static org.junit.Assert.*;
+
+public class GenericModuleTests {
+
+	private static void testFill(IParameterised p) {
+		// test that parameters and gradient can both be filled
+		p=p.clone();
+		
+		AVector param=p.getParameters();
+		AVector grad=p.getGradient();
+		
+		param.fill(1);
+		grad.fill(1);
+		assertTrue(param.epsilonEquals(grad));
+		
+		// extra logic should pick up any duplicated / repeated element references
+		for (int i=0; i<param.length(); i++) {
+			assertEquals(1.0, param.get(i),0.0);
+			param.set(i,2.0);
+		}
+		grad.add(grad);
+		assertTrue(param.epsilonEquals(grad));	
+	}
+	
+	private static void testCloneNotLinked(IParameterised p) {
+		// Test that a clone does not share any elements of the parameter or gardient vectors
+		p=p.clone();
+		
+		AVector param=p.getParameters();
+		AVector grad=p.getGradient();
+		param.fill(1.0);
+		grad.fill(1.0);	
+		
+		IParameterised p2=p.clone();
+		p2.getParameters().fill(2);
+		p2.getGradient().fill(2);
+		
+		assertEquals(1.0,Vectorz.maxValue(param),0.0);
+		assertEquals(1.0,Vectorz.maxValue(grad),0.0);
+	}
+	
+	private static void testCloneCopyParameters(IParameterised p) {
+		if (p instanceof IThinker) {
+			// Test that a clone does not share any elements of the parameter or gardient vectors
+			p=p.clone();
+			
+			AVector param=p.getParameters();
+			Vectorz.fillGaussian(param);
+			IParameterised p2=p.clone();
+			p2.getParameters().fill(2);
+			p2.getParameters().set(param);
+			
+			AVector input=Vectorz.newVector(((IThinker)p).getInputLength());
+			AVector output=Vectorz.newVector(((IThinker)p).getOutputLength());
+			AVector output2=Vectorz.newVector(((IThinker)p).getOutputLength());
+			
+			Vectorz.fillRandom(input);
+			((IThinker)p).think(input, output);
+			((IThinker)p2).think(input, output2);
+			assertTrue(output.epsilonEquals(output2));
+		}
+	}
+	
+	private static void testParameterized(IParameterised p) {
+		p=p.clone();
+		testFill(p);
+		testCloneNotLinked(p);
+		testCloneCopyParameters(p);
+	}
+	
+	private static void testOverwriteOutput(IThinker p) {
+		// test that output vector is completely overwritten by any IThinker
+		p=p.clone();
+		AVector input=Vectorz.newVector(p.getInputLength());
+		AVector output=Vectorz.newVector(p.getOutputLength());
+		
+		output.fill(Double.NaN);
+		p.think(input, output);
+		for (int i=0; i<output.length(); i++) {
+			assertTrue(output.get(i)!=Double.NaN);
+		}
+	}
+	
+	private static void testThinker(IThinker p) {
+		p=p.clone();
+		testOverwriteOutput(p);
+	}
+	
+	public static void test(Object o) {
+		if (o instanceof IParameterised) {
+			testParameterized((IParameterised)o);
+		}
+		if (o instanceof IThinker) {
+			testThinker((IThinker)o);
+		}
+	}
+}
---
+++ src/test/java/nuroko/module/TestSparseWeightLayer.java
@@ -0,0 +1,75 @@
+package nuroko.module;
+
+import static org.junit.Assert.assertEquals;
+import mikera.vectorz.AVector;
+import mikera.vectorz.Vector;
+import mikera.vectorz.Vectorz;
+
+import org.junit.Test;
+
+public class TestSparseWeightLayer {
+	@Test
+	public void testFullWeightLayer() {
+		SparseWeightLayer wl=new SparseWeightLayer(2,2,2);
+		
+		AVector params=wl.getParameters();
+		int pl=params.length();
+		assertEquals(6,pl);
+		
+		AVector input=Vectorz.createUniformRandomVector(2);
+		AVector output=Vectorz.createUniformRandomVector(2);
+		
+		// output should be zero
+		wl.think(input, output);
+		assertEquals(Vector.of(0,0),output);
+		
+		// identity weights
+		params.set(2,1);
+		params.set(5,1);
+		wl.think(input, output);
+		assertEquals(input,output);
+		
+		// constant weights
+		params.fill(0);
+		params.set(0,0.2);
+		params.set(1,0.3);
+		wl.think(input, output);
+		assertEquals(Vector.of(0.2,0.3),output);
+		
+		// all weights
+		input.fill(10);
+		params.fill(1);
+		wl.think(input, output);
+		assertEquals(Vector.of(21,21),output);
+	}
+	
+	@Test public void testInverse() {
+		SparseWeightLayer wl=new SparseWeightLayer(2,2,20);
+		AVector params=wl.getParameters();
+		int pl=params.length();
+		assertEquals(6,pl);
+		
+		Vectorz.fillGaussian(params);
+		
+		SparseWeightLayer inv=wl.getInverse();
+		AVector iparams=inv.getParameters();
+		assertEquals(6,iparams.length());
+		
+		// diagonal maps should be equal
+		assertEquals(params.get(2),iparams.get(2),0.0);
+		assertEquals(params.get(5),iparams.get(5),0.0);
+		
+		// cross maps should be swapped
+		assertEquals(params.get(3),iparams.get(4),0.0);
+		assertEquals(params.get(4),iparams.get(3),0.0);
+		
+		
+
+	}
+	
+	@Test
+	public void testGeneric() {
+		SparseWeightLayer wl=new SparseWeightLayer(2,2,2);
+		GenericModuleTests.test(wl);
+	}
+}
