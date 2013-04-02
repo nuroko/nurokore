a64a2a9ff1c7f007f36e4db6d0758e85c0ee9f2b
mikera <mike.r.anderson.13@gmail.com>
2013-04-02 11:31:34 +0800

    Add fullweightlayer and sparseweightlayer

---
+++ src/main/java/nuroko/module/FullWeightLayer.java
@@ -0,0 +1,161 @@
+package nuroko.module;
+
+import java.util.Collections;
+import java.util.List;
+
+import nuroko.core.IModule;
+
+import mikera.indexz.Index;
+import mikera.indexz.Indexz;
+import mikera.matrixx.AMatrix;
+import mikera.matrixx.impl.VectorMatrixMN;
+import mikera.vectorz.AVector;
+import mikera.vectorz.Vector;
+import mikera.vectorz.Vectorz;
+
+/**
+ * Fully connected weight layer
+ * 
+ * @author Mike
+ */
+public final class FullWeightLayer extends AWeightLayer {
+	private final Vector bias;
+	private final Vector biasGradient;
+	private final Vector[] weights;
+	private final Vector[] weightGradients;
+	private final AVector parameters;
+	private final AVector gradient;
+	
+	public FullWeightLayer(int inputLength, int outputLength) {
+		super(inputLength,outputLength);
+		
+		bias=Vector.createLength(outputLength);
+		weights=new Vector[outputLength];
+		
+		AVector params=bias;
+		for (int j=0; j<outputLength; j++) {
+			Vector wts=Vector.createLength(inputLength);
+			weights[j]=wts;			
+			params=params.join(wts);
+		}
+		parameters=params;
+		
+		biasGradient=Vector.createLength(outputLength);
+		weightGradients=new Vector[outputLength];
+		
+		AVector g=biasGradient;
+		for (int j=0; j<outputLength; j++) {
+			Vector grd=Vector.createLength(inputLength);
+			weightGradients[j]=grd;
+			g=g.join(grd);
+		}
+		gradient=g;
+		assert(gradient.length()==parameters.length());
+	}
+	
+	
+	@Override
+	public AVector getParameters() {
+		return parameters;
+	}
+	
+	@Override 
+	public List<IModule> getComponents() {
+		return Collections.EMPTY_LIST;
+	}
+	
+	@Override
+	public int getParameterLength() {
+		return parameters.length();
+	}
+
+	@Override
+	public void think(AVector input, AVector output) {
+		assert(inputLength==input.length());
+		assert(outputLength==output.length());
+		for (int j=0; j<outputLength; j++) {
+			double val=bias.get(j);
+			val+=weights[j].dotProduct(input);
+			output.set(j,val);
+		}
+	}
+
+	@Override
+	public AVector getGradient() {
+		return gradient;
+	}
+
+	@Override
+	public void trainGradient(AVector input,
+			AVector outputGradient, AVector inputGradient, double factor) {
+		biasGradient.addMultiple(outputGradient,factor);
+		for (int j=0; j<outputLength; j++) {
+			double grad=outputGradient.get(j);
+			weightGradients[j].addMultiple(input, grad*factor);
+			inputGradient.addMultiple(weights[j],grad);
+		}
+	}
+	
+	@Override 
+	public FullWeightLayer clone() {
+		FullWeightLayer wl=new FullWeightLayer(getInputLength(), getOutputLength());
+		wl.getParameters().set(this.getParameters());
+		return wl;
+	}
+
+	@Override
+	public int getLinkCount(int outputIndex) {
+		return inputLength;
+	}
+
+	@Override
+	public double getLinkWeight(int outputIndex, int number) {
+		return weights[outputIndex].array[number];
+	}
+
+	@Override
+	public int getLinkSource(int outputIndex, int number) {
+		return number;
+	}
+	
+	@Override
+	public void initRandom() {
+		Vectorz.fillGaussian(bias, 0.0, BIAS_INITIAL_SCALE* INITIAL_WEIGHT_SCALE);
+		for (Vector v:weights) {
+			Vectorz.fillGaussian(v, 0.0, INITIAL_WEIGHT_SCALE/(Math.sqrt(v.length())));
+		}
+	}
+	
+	@Override public FullWeightLayer getInverse() {
+		FullWeightLayer wl=getInverseStructure();
+		for (int j=0; j<outputLength; j++) {
+			AVector owts=getSourceWeights(j);
+			for (int i=0; i<inputLength; i++) {
+				AVector wts=wl.getSourceWeights(i);
+				wts.set(j,owts.get(i));
+			}
+		}
+		return wl;
+	}
+	
+	public FullWeightLayer getInverseStructure() {
+		FullWeightLayer wl=new FullWeightLayer(getOutputLength(),getInputLength());
+		return wl;
+	}
+
+	@Override
+	public Index getSourceIndex(int outputIndex) {
+		return Indexz.createSequence(inputLength);
+	}
+
+	@Override
+	public AVector getSourceWeights(int outputIndex) {
+		return weights[outputIndex];
+	}
+
+
+	@Override
+	public AMatrix asMatrix() {
+		return VectorMatrixMN.wrap(weights);
+	}
+}
---
+++ src/main/java/nuroko/module/SparseWeightLayer.java
@@ -0,0 +1,228 @@
+package nuroko.module;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import nuroko.core.IModule;
+
+import mikera.indexz.Index;
+import mikera.indexz.Indexz;
+import mikera.matrixx.AMatrix;
+import mikera.matrixx.Matrixx;
+import mikera.vectorz.AVector;
+import mikera.vectorz.GrowableVector;
+import mikera.vectorz.Vector;
+import mikera.vectorz.Vectorz;
+
+/**
+ * Fully connected weight layer
+ * 
+ * @author Mike
+ */
+public final class SparseWeightLayer extends AWeightLayer {
+	private final Vector bias;
+	private final Vector biasGradient;
+	private final Index[] indexes;
+	private final Vector[] weights;
+	private final Vector[] weightGradients;
+	private AVector parameters;
+	private AVector gradient;
+	
+	public SparseWeightLayer(int inputLength, int outputLength, int maxLinks) {
+		super(inputLength,outputLength);
+		
+		bias=Vector.createLength(outputLength);
+		weights=new Vector[outputLength];
+		indexes=new Index[outputLength];
+
+		int links=Math.min(maxLinks, inputLength);
+		
+		AVector params=bias;
+		for (int j=0; j<outputLength; j++) {
+			Vector wts=Vector.createLength(links);
+			Index inds=Indexz.createRandomChoice(links, inputLength);
+			weights[j]=wts;			
+			indexes[j]=inds;			
+			params=params.join(wts);
+		}
+		parameters=params;
+		
+		biasGradient=Vector.createLength(outputLength);
+		weightGradients=new Vector[outputLength];
+		
+		AVector g=biasGradient;
+		for (int j=0; j<outputLength; j++) {
+			Vector grd=Vector.createLength(weights[j].length());
+			weightGradients[j]=grd;
+			g=g.join(grd);
+		}
+		gradient=g;
+		assert(gradient.length()==parameters.length());
+	}
+	
+	public SparseWeightLayer(SparseWeightLayer wl) {
+		super(wl.inputLength,wl.outputLength);
+		bias=wl.bias.clone();
+		biasGradient=wl.biasGradient.clone();
+		
+		weights=new Vector[outputLength];
+		indexes=new Index[outputLength];
+		
+		AVector params=bias;
+		for (int j=0; j<outputLength; j++) {
+			Vector wts=wl.weights[j].clone();
+			Index inds=wl.indexes[j].clone();
+			weights[j]=wts;			
+			indexes[j]=inds;			
+			params=params.join(wts);
+		}
+		parameters=params;
+		
+		weightGradients=new Vector[biasGradient.length()];
+		
+		AVector g=biasGradient;
+		for (int j=0; j<outputLength; j++) {
+			Vector grd=wl.weightGradients[j].clone();
+			weightGradients[j]=grd;
+			g=g.join(grd);
+		}
+		gradient=g;
+		assert(gradient.length()==parameters.length());
+
+	}
+	
+	@Override 
+	public List<IModule> getComponents() {
+		return Collections.EMPTY_LIST;
+	}
+	
+	@Override
+	public AMatrix asMatrix() {
+		return Matrixx.createSparse(getInputLength(),indexes,weights);
+	}
+
+	@Override
+	public AVector getParameters() {
+		return parameters;
+	}
+	
+	
+	@Override
+	public int getParameterLength() {
+		return parameters.length();
+	}
+
+	@Override
+	public void think(AVector input, AVector output) {
+		assert(inputLength==input.length());
+		assert(outputLength==output.length());
+		for (int i=0; i<outputLength; i++) {
+			double val=bias.get(i);
+			Vector wts=weights[i];
+			Index inds=indexes[i];
+			val+=input.dotProduct(wts, inds);
+			output.set(i,val);
+		}
+	}
+
+	@Override
+	public AVector getGradient() {
+		return gradient;
+	}
+
+	@Override
+	public void trainGradient(AVector input, AVector outputGradient, AVector inputGradient, double factor) {
+		biasGradient.addMultiple(outputGradient,factor);
+		for (int j=0; j<outputLength; j++) {
+			double grad=outputGradient.get(j);
+			weightGradients[j].addMultiple(input, grad*factor);
+			inputGradient.addMultiple(weights[j], indexes[j],grad);
+		}
+	}
+	
+	@Override 
+	public SparseWeightLayer clone() {
+		SparseWeightLayer wl=new SparseWeightLayer(this);
+		wl.getParameters().set(this.getParameters());
+		return wl;
+	}
+
+	@Override
+	public int getLinkCount(int outputIndex) {
+		return weights[outputIndex].length();
+	}
+
+	@Override
+	public double getLinkWeight(int outputIndex, int number) {
+		return weights[outputIndex].array[number];
+	}
+
+	@Override
+	public int getLinkSource(int outputIndex, int number) {
+		return indexes[outputIndex].data[number];
+	}
+
+	@Override
+	public void initRandom() {
+		Vectorz.fillGaussian(bias, 0.0, BIAS_INITIAL_SCALE* INITIAL_WEIGHT_SCALE);
+		for (Vector v:weights) {
+			Vectorz.fillGaussian(v, 0.0, INITIAL_WEIGHT_SCALE/(Math.sqrt(v.length())));
+		}
+	}
+
+	@Override
+	public Index getSourceIndex(int outputIndex) {
+		return indexes[outputIndex];
+	}
+
+	@Override
+	public AVector getSourceWeights(int outputIndex) {
+		return weights[outputIndex];
+	}
+
+	@Override
+	public SparseWeightLayer getInverse() {
+		int inps=getInputLength();
+		int outps=getOutputLength();
+		SparseWeightLayer wl=new SparseWeightLayer(getOutputLength(),getInputLength(),0);
+		
+		GrowableVector[] weightVectors=new GrowableVector[inps];
+		@SuppressWarnings("unchecked")
+		ArrayList<Integer>[] indexVectors = new ArrayList[inps];
+		for (int i=0; i<inps; i++) {
+			weightVectors[i]=new GrowableVector();
+			indexVectors[i]=new ArrayList<Integer>();
+		}
+		
+		for (int j=0; j<outps; j++) {
+			AVector owts=getSourceWeights(j);
+			Index oixs=getSourceIndex(j);
+			assert(owts.length()==oixs.length());
+			for (int i=0; i<owts.length(); i++) {
+				int si=oixs.get(i);
+				indexVectors[si].add(j);
+				weightVectors[si].append(owts.get(i));	
+			}			
+		}	
+		
+		for (int i=0; i<inps; i++) {
+			wl.weights[i]=new Vector(weightVectors[i]);
+			wl.indexes[i]=Indexz.create(indexVectors[i]);
+			wl.weightGradients[i]=Vector.createLength(wl.weights[i].length());
+		}
+		wl.rebuildVectors();
+		return wl;
+	}
+
+	private void rebuildVectors() {
+		AVector params=bias;
+		AVector grads=biasGradient;
+		for (int i=0; i<outputLength; i++) {
+			params=params.join(weights[i]);
+			grads=grads.join(weightGradients[i]);
+		}
+		parameters=params;
+		gradient=grads;
+	}
+}
