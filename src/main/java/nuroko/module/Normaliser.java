1a5c59aa53ba423233c2b9a4a951db798f329b34
mikera <mike.r.anderson.13@gmail.com>
2013-04-08 17:39:39 +0800

    make Normaliser work as synth

--- src/main/java/nuroko/module/Normaliser.java
+++ src/main/java/nuroko/module/Normaliser.java
@@ -3,11 +3,12 @@ package nuroko.module;
 import java.util.List;
 
 import nuroko.core.IComponent;
+import nuroko.core.ISynthesiser;
 import mikera.vectorz.AVector;
 import mikera.vectorz.Vector;
 import mikera.vectorz.impl.Vector0;
 
-public class Normaliser extends AStateComponent {
+public class Normaliser extends AStateComponent implements ISynthesiser {
 	private final Vector mean;
 	private final Vector stdev;
 	
@@ -54,11 +55,26 @@ public class Normaliser extends AStateComponent {
 	}
 
 	@Override
-	public AComponent clone() {
+	public Normaliser clone() {
 		Normaliser n=new Normaliser(getInputLength());
 		n.mean.set(mean);
 		n.stdev.set(stdev);
 		return n;
+	}
+
+	@Override
+	public void generate(AVector input, AVector output) {
+		this.output.set(output);
+		this.input.set(output);
+		this.input.multiply(stdev);
+		this.input.add(mean);
+		input.set(this.input);
+	}
+
+	@Override
+	public void trainSynth(AVector input) {
+		this.input.set(input);
+		thinkInternal();
 	}
 
 }
