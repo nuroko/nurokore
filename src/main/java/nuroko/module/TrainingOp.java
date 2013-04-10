e032d9533887bfcd3c942bafd6457b85a2bff87c
mikera <mike.r.anderson.13@gmail.com>
2013-04-10 14:46:27 +0800

    add TrainingOp component

---
+++ src/main/java/nuroko/module/TrainingOp.java
@@ -0,0 +1,12 @@
+package nuroko.module;
+
+import mikera.vectorz.Op;
+
+public abstract class TrainingOp extends AOperationComponent {
+
+	public TrainingOp(int length, Op  op) {
+		super(length);
+		// TODO Auto-generated constructor stub
+	}
+
+}
