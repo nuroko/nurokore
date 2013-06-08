1045cce31d016158a04644d11a03ab4bb87b1903
mikera <mike.r.anderson.13@gmail.com>
2013-06-08 11:43:42 +0800

    use default loss function

--- src/main/java/nuroko/algo/SimpleBackProp.java
+++ src/main/java/nuroko/algo/SimpleBackProp.java
@@ -3,11 +3,10 @@ package nuroko.algo;
 import mikera.vectorz.AVector;
 import nuroko.core.IComponent;
 import nuroko.module.loss.LossFunction;
-import nuroko.module.loss.SquaredErrorLoss;
 
 public class SimpleBackProp {
 	public static void train(IComponent comp, AVector input, AVector target,double learnRate) {
-		train(comp,input,target,learnRate,SquaredErrorLoss.INSTANCE);
+		train(comp,input,target,learnRate,comp.getDefaultLossFunction());
 	}
 	
 	public static void train(IComponent comp, AVector input, AVector target,double learnRate, LossFunction loss) {
