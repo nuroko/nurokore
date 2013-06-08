65a0ead53464e88ab03d7682643b0c719cf4b65d
mikera <mike.r.anderson.13@gmail.com>
2013-06-08 12:34:30 +0800

    make decode/enocde functions implement IEncoder and IDecoder

--- src/main/java/nuroko/coders/DecodeFunction.java
+++ src/main/java/nuroko/coders/DecodeFunction.java
@@ -2,9 +2,10 @@ package nuroko.coders;
 
 import mikera.vectorz.AVector;
 import nuroko.core.ICoder;
+import nuroko.core.IDecoder;
 import nuroko.core.IFunction;
 
-public class DecodeFunction<T> implements IFunction <AVector,T>{
+public class DecodeFunction<T> implements IFunction <AVector,T>, IDecoder<T> {
 
 	private final ICoder<T> coder;
 	
@@ -15,6 +16,17 @@ public class DecodeFunction<T> implements IFunction <AVector,T>{
 	@Override
 	public T think(AVector input) {
 		return coder.decode(input, 0);
+	}
+
+	@Override
+	public T decode(AVector v, int offset) {
+		return coder.decode(v, offset);
+	}
+
+	@Override
+	public int codeLength() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
--- src/main/java/nuroko/coders/EncodeFunction.java
+++ src/main/java/nuroko/coders/EncodeFunction.java
@@ -3,9 +3,10 @@ package nuroko.coders;
 import mikera.vectorz.AVector;
 import mikera.vectorz.Vector;
 import nuroko.core.ICoder;
+import nuroko.core.IEncoder;
 import nuroko.core.IFunction;
 
-public class EncodeFunction<T> implements IFunction <T,AVector>{
+public class EncodeFunction<T> implements IFunction <T,AVector>, IEncoder<T> {
 
 	private final ICoder<T> coder;
 	
@@ -18,5 +19,22 @@ public class EncodeFunction<T> implements IFunction <T,AVector>{
 		Vector v=Vector.createLength(coder.codeLength());
 		coder.encode(input, v, 0) ;
 		return v;
+	}
+
+	@Override
+	public void encode(T object, AVector dest, int offset) {
+		coder.encode(object, dest, offset);
+	}
+
+	@Override
+	public int codeLength() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public AVector createOutputVector() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
