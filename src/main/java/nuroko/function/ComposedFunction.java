e5079bf7a5701f0108fcdcbabeb324fa2b52f9f3
mikera <mike.r.anderson.13@gmail.com>
2013-06-09 12:04:28 +0800

    add ComposedFunction

---
+++ src/main/java/nuroko/function/ComposedFunction.java
@@ -0,0 +1,22 @@
+package nuroko.function;
+
+import nuroko.core.IFunction;
+
+public class ComposedFunction <A,B> implements IFunction<A,B> {
+
+	public final IFunction<A,Object> first;
+	public final IFunction<Object,B> second;
+	
+	@SuppressWarnings("unchecked")
+	public ComposedFunction(IFunction<A,?> a, IFunction<?,B> b) {
+		first=(IFunction<A, Object>) a;
+		second=(IFunction<Object, B>) b;
+		
+	}
+	
+	@Override
+	public B think(A input) {
+		return second.think(first.think(input));
+	}
+
+}
