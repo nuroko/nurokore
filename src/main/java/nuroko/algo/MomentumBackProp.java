f1871aa1974e77bf56691834bbcf5bb59574421f
mikera <mike.r.anderson.13@gmail.com>
2013-06-10 11:07:56 +0100

    momentum backprop algorithm

---
+++ src/main/java/nuroko/algo/MomentumBackProp.java
@@ -0,0 +1,33 @@
+package nuroko.algo;
+
+import mikera.vectorz.AVector;
+import mikera.vectorz.Vector;
+import nuroko.core.IComponent;
+import nuroko.module.AComponent;
+import nuroko.module.loss.LossFunction;
+
+public class MomentumBackProp {
+	private Vector lastUpdate;
+	private double momentum;
+
+	public MomentumBackProp(AComponent comp, double momentum) {
+		this(comp.getParameterLength(), momentum);
+	}
+	
+	public MomentumBackProp(int paramLength, double momentum) {
+		lastUpdate = Vector.createLength(paramLength);
+		this.momentum=momentum;
+	}
+	
+	public void train(IComponent comp, AVector input, AVector target,double learnRate) {
+		train(comp,input,target,learnRate,comp.getDefaultLossFunction());
+	}
+	
+	public void train(IComponent comp, AVector input, AVector target,double learnRate, LossFunction loss) {
+		comp.getGradient().fill(0.0);
+		comp.train(input,target,loss,1.0);
+		lastUpdate.multiply(1.0-momentum);
+		lastUpdate.addMultiple(comp.getGradient(),learnRate);
+		comp.getParameters().add(lastUpdate);
+	}
+}
