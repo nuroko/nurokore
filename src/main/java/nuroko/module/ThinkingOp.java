f000487ae978de85a7d8c556aaf3dbdcf03cc634
mikera <mike.r.anderson.13@gmail.com>
2013-06-10 15:57:41 +0100

    add thinkingop

---
+++ src/main/java/nuroko/module/ThinkingOp.java
@@ -0,0 +1,44 @@
+package nuroko.module;
+
+import mikera.vectorz.Op;
+
+public class ThinkingOp extends AOperationComponent {
+	private final Op op;
+	
+	public ThinkingOp(int length, Op  op) {
+		super(length);
+		this.op = op;
+	}
+
+	@Override
+	public void thinkInternal() {
+		// apply op only when thinking
+		output.set(input);
+		op.applyTo(output);
+	}
+	
+	@Override
+	public void thinkInternalTraining() {
+		output.set(input);
+	}
+
+	@Override
+	public void trainGradientInternal(double factor) {
+		inputGradient.set(outputGradient);
+	}
+	
+	@Override
+	public boolean hasDifferentTrainingThinking() {
+		return true;
+	}
+
+	@Override
+	public ThinkingOp clone() {
+		return new ThinkingOp(length,op);
+	}
+	
+	@Override public String toString() {
+		return length+":"+length+" "+op.toString();
+	}
+
+}
